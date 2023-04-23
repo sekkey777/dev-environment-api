558f65073298c172c9b6d0ced0adaab4e4f0a57b
sekkey777 <seki7united@gmail.com>
2023-04-24 00:04:41 +0900

    add/update：新規作成・更新

---
+++ frontend/app/src/components/Icons/index.js
@@ -0,0 +1,4 @@
+// --- ここから追加 ---
+export { default as LocalMallIcon } from '@mui/icons-material/LocalMall';
+export { default as QueryBuilderIcon } from '@mui/icons-material/QueryBuilder';
+// --- ここまで追加 ---
